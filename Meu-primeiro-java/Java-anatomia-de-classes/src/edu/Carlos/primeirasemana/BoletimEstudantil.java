bd49e5b28a7a14f656c8e210720563d6c521f2c0
Carlos Eduardo <carlos95edu.s@gmail.com>
2023-07-10 10:27:39 -0300

    tipos e variaveis

---
+++ Meu-primeiro-java/Java-anatomia-de-classes/src/edu/Carlos/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,13 @@
+package edu.Carlos.primeirasemana;
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 5;
+            if (mediaFinal<6)
+                System.out.println ("REPROVADO");
+            else if (mediaFinal == 6)
+                System.out.println ("Prova Recuperação");
+            else 
+                System.out.println ("Aprovado");
+
+    }
+}
---
+++ Meu-primeiro-java/Java-anatomia-de-classes/src/edu/Carlos/segundasemana/MinhaClasse.java
@@ -0,0 +1,39 @@
+package edu.Carlos.segundasemana;
+public class MinhaClasse {
+    public static void main (String [] args)
+    {
+        System.out.print ("Olá mundo" +"\n");
+                        //Variaveis jamais deve iniciar com numero//
+                        //unicos simbolos permitidos _ $
+                        // int ano_2000 = 2021;
+                        //ano_2000 = 2022;
+                        //tipo nome = valor//
+                        // int anoFabricacao = 2020;
+                        // String motoHonda = "Bross";
+                        
+                        // Variaveis improprias
+                        // int numero um = 1;
+                        // int %numero1 = 1;
+                        //Corretas
+                        // int numeroum = 1;
+                        // int $numero1 = 1;
+
+                        // final String BR = "Brasil";
+                        //final Não permite que a variavel seja alterada 
+                        //como a variavel "ano" ou valor de PI=3,14
+                    
+        System.out.print ("Braaaaaaaaaazil"+"\n");
+        String primeiroNome = "Carlos";
+        String segundoNome = "Eduardo";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.print(nomeCompleto);
+    }    
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome)
+    {
+        return "Resultado método " + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+
+}
+
